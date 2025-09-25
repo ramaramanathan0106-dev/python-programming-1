7dcbd705e6df0f2c006f9463fc7c3e09022607c2
ramaramanathan0106-dev <ramaramanathan0106@gmail.com>
2025-09-25 10:51:09 +0530

    25/09/25

---
+++ syst/syst.py
@@ -0,0 +1,14 @@
+pas = int(input("enter your password:"))
+ 
+while True:
+          if (pas==2003):
+             print("correct password")
+             print("withdrawl or deposit")
+             break
+          else:
+             print("invalid")
+             break
+          
+          
+    
+
